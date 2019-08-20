5af7b04cbb5709147da1667abfaa031e5d8ae923
Fanatec Desenvolvimento <unitecdesenvolvimento@gmail.com>
2019-08-19 21:27:02 -0300

    Projeto recriado pois estava com erro nos arquivos XML. Em pleno funcionamento.

---
+++ app/src/main/java/br/com/fanatec/appdesenvolvaseufilho/CadastroUsuario.java
@@ -0,0 +1,4 @@
+package br.com.fanatec.appdesenvolvaseufilho;
+
+public class CadastroUsuario {
+}
---
+++ app/src/main/java/br/com/fanatec/appdesenvolvaseufilho/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.fanatec.appdesenvolvaseufilho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
